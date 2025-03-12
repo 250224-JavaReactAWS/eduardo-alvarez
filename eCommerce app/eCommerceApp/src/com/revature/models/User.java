b275219c2048fddb9a14e5e84a223d58fd5d91f3
d3val <devalt5008@gmail.com>
2025-03-12 12:05:38 -0600

    inital commit

---
+++ eCommerce app/eCommerceApp/src/com/revature/models/User.java
@@ -0,0 +1,65 @@
+package com.revature.models;
+
+public class User {
+    private int userID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Role role;
+
+    public  User(String fName, String lName, String email, String password){
+        firstName = fName;
+        lastName = lName;
+        this.email=email;
+        this.password = password;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
