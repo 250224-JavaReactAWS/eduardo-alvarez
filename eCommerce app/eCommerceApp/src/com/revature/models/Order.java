0d0bfec0782e7b29e16064237568a72bec3c5e1c
d3val <devalt5008@gmail.com>
2025-03-13 19:27:49 -0600

    Models added

---
+++ eCommerce app/eCommerceApp/src/com/revature/models/Order.java
@@ -0,0 +1,8 @@
+package com.revature.models;
+
+public class Order {
+    private int orderID;
+    private int userID;
+    private  float total_price;
+    private OrderStatus status;
+}
---
+++ eCommerce app/eCommerceApp/src/com/revature/models/OrderItem.java
@@ -0,0 +1,9 @@
+package com.revature.models;
+
+public class OrderItem {
+    private int orderItemID;
+    private int orderID;
+    private int productID;
+    private int quantity;
+    private float price;
+}
