33d5052e7ed39637b06cb0d1e28331478731f073
d3val <devalt5008@gmail.com>
2025-03-19 13:44:38 -0600

    connection properties removed from code

--- eCommerce/src/main/java/com/revature/util/ConnectionUtil.java
+++ eCommerce/src/main/java/com/revature/util/ConnectionUtil.java
@@ -1,8 +1,11 @@
 package com.revature.util;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionUtil {
     private static Connection conn = null;
@@ -23,16 +26,26 @@ public class ConnectionUtil {
 
 
 
-        String url ="jdbc:postgresql://project-ecommerce.cveeweuksxh9.us-east-2.rds.amazonaws.com:5432/postgres";
-        String username = "postgres";
-        String password = "Sora1698..";
+        String url;
+        String username;
+        String password;
+
+        Properties properties = new Properties();
 
         try {
+            properties.load(new FileReader("src/main/resources/application.properties"));
+            url = properties.getProperty("url");
+            username = properties.getProperty("username");
+            password = properties.getProperty("password");
+
             conn = DriverManager.getConnection(url, username, password);
             System.out.println("Conexion establecida");
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("No se pudo conectar");
+        }catch (IOException e){
+            e.printStackTrace();
+            System.out.println("No se pudo leer propiedades");
         }
         return conn;
     }
