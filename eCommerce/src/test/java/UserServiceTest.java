c6c5bf350dab24cd5b17af14fcdeb7f00ad631e9
d3val <devalt5008@gmail.com>
2025-03-23 13:29:50 -0600

    Mockito dependencies added

--- eCommerce/src/test/java/UserServiceTest.java
+++ eCommerce/src/test/java/UserServiceTest.java
@@ -1,7 +1,29 @@
+import com.revature.repos.UserDAO;
+import com.revature.repos.UserDAOPostgres;
+import com.revature.services.UserService;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class UserServiceTest {
+    private UserDAO userDAO = new UserDAOPostgres();
+    private UserService userService = new UserService(userDAO);
 
     @Test
-    public void
+    public void validEmail(){
+        String email = "MyEmail@gmail.com";
+
+        boolean result = userService.validateEmail(email);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void invalidEmail(){
+        String email = "MyEmailgmail.com";
+
+        boolean result = userService.validateEmail(email);
+
+        assertFalse(result);
+    }
 }
